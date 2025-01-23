e0b8f9b3295400660d82024eb7024a18451f5e97
AnastasiaPolishchuk <zipz231_pai@student.ztu.edu.ua>
2025-01-23 17:32:57 +0700

    Оновлено консольний проект

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть перше число: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Введіть друге число: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int sum = num1 + num2;
+        Console.WriteLine($"Сума: {sum}");
+    }
+}
